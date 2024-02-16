72757ab2e3a9c94b3f13a6ebb2aee5e1b137a2f2
Giya077 <giyaDevIos@gmail.com>
2024-02-16 15:51:47 +0600

    Профиль. Добавил второй экран.

---
+++ ImagesList/Profile/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by GiyaDev on 16.02.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+                    
+    @IBOutlet var logoutButton: UIButton!
+    
+    @IBOutlet var nameLabel: UILabel!
+    
+    @IBOutlet var loginLabel: UILabel!
+    
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    @IBOutlet var avatarImage: UIImageView!
+    
+    @IBAction func didTapLogoutButton(_ sender: Any) {
+    }
+}
