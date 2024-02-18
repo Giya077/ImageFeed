f582bb2f92bf33b95b5120eb1456b0b58de6bd52
Giya077 <giyaDevIos@gmail.com>
2024-02-18 16:59:14 +0600

    сделал UINavigationController,добавил новый контроллер Single Image View

---
+++ Single Image/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by GiyaDev on 18.02.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
