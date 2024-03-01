d89b9ae72467aab5d25280503ccaaf85be3a572c
Giya077 <giyaDevIos@gmail.com>
2024-03-01 14:59:38 +0600

    добавил файл enum Constants

---
+++ Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by GiyaDev on 01.03.2024.
+//
+
+import Foundation
+
+
+enum Constants {
+    static let accessKey = "wnmabgtUsRiGP9nzEKk9hHIBrRAznuL6o5HRKpPfw8k"
+    static let secretKey = "Qe6Dy9MCXyUz27wuLdGKNFvyxy3Ppc7mmXcA3aGytaM"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com/")
+}
