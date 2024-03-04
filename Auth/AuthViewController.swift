787da7c33943a3b0e2085f53b4d5df1805529390
Giya077 <giyaDevIos@gmail.com>
2024-03-04 21:55:50 +0600

    Реализовал Protocol,Delegate

--- Auth/AuthViewController.swift
+++ Auth/AuthViewController.swift
@@ -10,44 +10,76 @@ import UIKit
 
 final class AuthViewController: UIViewController {
     
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowWebViewSegueIdentifier {
+            guard
+                let webViewViewController = segue.destination as? WebViewViewController
+            else { fatalError("Failed to prepare for \(ShowWebViewSegueIdentifier)") }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureBackButton()
         view.backgroundColor = .ypBlack
         
-        //IMAGE
-        
-        let logo = UIImage(named: "auth_screen_logo")
-        let imageView = UIImageView(image: logo)
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "auth_screen_logo")
         view.addSubview(imageView)
         
         NSLayoutConstraint.activate([
-        imageView.heightAnchor.constraint(equalToConstant: 60),
-        imageView.widthAnchor.constraint(equalToConstant: 60),
-        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            imageView.heightAnchor.constraint(equalToConstant: 60),
+            imageView.widthAnchor.constraint(equalToConstant: 60),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
         
-        //BUTTON
-        
-        let loginButton = UIButton()
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(loginButton)
-        
         loginButton.backgroundColor = .white
         loginButton.setTitleColor(.black, for: .normal)
         loginButton.setTitle("Войти", for: .normal)
-        loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        loginButton.titleLabel?.font = UIFont(name: "YS Display Bold", size: 23)
         loginButton.layer.cornerRadius = 16
         loginButton.layer.masksToBounds = true
         
         NSLayoutConstraint.activate([
-        loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-        loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-        loginButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
-        loginButton.heightAnchor.constraint(equalToConstant: 50)
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            loginButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            loginButton.heightAnchor.constraint(equalToConstant: 50)
         ])
+    }
+    
+    //    @objc func loginButtonTapped() {
+    //        let webViewVC = WebViewViewController()
+    //        navigationController?.pushViewController(webViewVC, animated: true)
+    //    }
+    
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back_button")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = UIColor.ypBlack
+    }
+}
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        //TODO: process code
+    }
+    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
     }
 }
---
+++ Auth/WebViewViewController.swift
@@ -0,0 +1,102 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by GiyaDev on 03.03.2024.
+//
+
+import Foundation
+import UIKit
+import WebKit
+
+enum WebViewConstants {
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
+
+protocol WebViewViewControllerDelegate: AnyObject {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController)
+}
+
+final class WebViewViewController: UIViewController {
+    
+    @IBOutlet private var webView: WKWebView!
+    
+    weak var delegate: WebViewViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.navigationDelegate = self //навигационный делегат
+        view.addSubview(webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.backgroundColor = .white
+        
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        loadAuthView()
+    }
+    
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
+            print()
+            return
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        
+        guard let url = urlComponents.url else {
+            print()
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+    
+    
+    
+    @IBAction private func didTapBackButton(_ sender: Any?) {
+        delegate?.webViewViewControllerDidCancel(self)
+    }
+}
+
+extension WebViewViewController: WKNavigationDelegate {
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+    ) {
+        if let code = code(from: navigationAction) {
+            delegate?.webViewViewController(self, didAuthenticateWithCode: code)
+            decisionHandler(.cancel)
+        } else {
+            decisionHandler(.allow)
+        }
+    }
+    
+    private func code(from navigationAction: WKNavigationAction) -> String? {
+        if
+            let url = navigationAction.request.url,
+            let urlComponents = URLComponents(string: url.absoluteString),
+            urlComponents.path == "/oauth/authorize/native",
+            let items = urlComponents.queryItems,
+            let codeItem = items.first(where: { $0.name == "code" })
+        {
+            return codeItem.value
+        } else {
+            return nil
+        }
+    }
+}
+
